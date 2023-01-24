22b83e18be55fb9ab6f0cda04f87169d8c6854bb
rohan <rohan.kumar@celebaltech.com>
2023-01-24 16:55:44 +0530

    updates package.json with build script

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.json({ message: "I'm active" })
 })
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("listening....");
 });
