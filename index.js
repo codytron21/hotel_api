e82daea11a9f7c3ba27c2cfa7daa63baa5830eed
rohan <rohannak2057@gmail.com>
2023-01-24 13:25:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: "I'm active" })
+})
+app.listen(process.env.PORT || 3000, () => {
+    console.log("listening....");
+});
